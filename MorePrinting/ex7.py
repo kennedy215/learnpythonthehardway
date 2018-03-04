34dd0daade9d057be2b40623f66b47dc1d1bcaa5
Kennedy DeSousa <kennedydesousa@gmail.com>
2018-03-03 16:42:41 -0800

    excercise 7 complete

---
+++ MorePrinting/ex7.py
@@ -0,0 +1,33 @@
+print "Mary had a little lamb."
+print "It's fleece was white as %s." %'snow'
+print "And everywhere that Mary went."
+print "." * 10  #what'd that do
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end. try removing it to see
+# what happens
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
+
+#Study drills
+
+# 1. Go back through and write a comment on what each line does.
+# 2. Read each one backwards or out loud to find your errors.
+# 3. From now on, when you make mistakes, write down on a piece of paper what kind of mistake you made
+# 4. When you go to the next exercise, look at the last mistake you made and Try
+# 5. not to make them in this new one.
+# 6. Remember that everyone makes mistakes. Programmers are like magicians who like
+# everyone to think they are perfect and never wrong, but it's all an act. they
+# make mistakes all the time. 
