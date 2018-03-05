f3676c1c5e3c00f97db0b036d68adb110fa820f6
Kennedy DeSousa <kennedydesousa@gmail.com>
2018-03-05 12:11:52 -0800

    example 12 complete

---
+++ PromptingPeople/ex12.py
@@ -0,0 +1,15 @@
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you? ")
+weight = raw_input("How much do you weight? ")
+
+print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
+
+# Study Drills
+# 1. In Terminal, where you normall run python to run your scripts, type
+# pydoc raw_input instead
+# 2. Get out of pydoc by typing q to quit
+# 3. Look online for what the pydoc command does.
+
+# 4. Use pydoc to also read about open, file, os and sys. It's alright if you
+# do not understand those; just read through and take notes about interesting things
+# Using pydoc is great! 
