8b3f132701d9f53fe2cbb9265a7aae438596a5d3
Kennedy DeSousa <kennedydesousa@gmail.com>
2018-03-09 08:50:20 -0800

    ex17.py in progress

--- MoreFiles/ex17.py
+++ MoreFiles/ex17.py
@@ -3,9 +3,9 @@ from os.path import exists
 
 script, from_file, to_file = argv
 
-print "Copying from %s o %s" % (from_file, to_file)
+print "Copying from %s to %s" % (from_file, to_file)
 
-#we could do these two on one line too, how?
+# we could do these two on one line too, how?
 in_file = open(from_file)
 indata = in_file.read()
 
@@ -22,3 +22,17 @@ print "Alright, all done."
 
 out_file.close()
 in_file.close()
+
+# Study Drills
+
+# 1.Go read up on Python's import statmeent, and start python to try it out
+# 2. This scrit is really annoying. There's no need to ask you before
+# doing the copy, and it prints too much to the screen. Try to make it
+# More friendly to use by removing features.
+# 3. See how show you can make the script I could make this one line long
+# 4. Notice at the end of the WYSS I used something called cat?
+# It's an old command that "concatenates" files together, but mostly it's
+# just an easy way to print a file to the screen. type man cat to read about it
+# 5. Windows, people find the alternative to cat that Linux/OSX people have
+# Do not worry about man since there is nothing like that.
+# Find out why you had to do output.close() in the code
