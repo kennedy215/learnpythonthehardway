2b28616e2423b89f93bcdc853d238477648d1775
Kennedy DeSousa <kennedydesousa@gmail.com>
2018-03-13 09:44:34 -0700

    Added Study Drills and README

--- PrintingPrinting/ex8.py
+++ PrintingPrinting/ex8.py
@@ -7,10 +7,19 @@ print formatter % ("one","two","three","four")
 print formatter % (True, False, False, True)
 # repeat '%r,%r,%r,%r','%r,%r,%r,%r','%r,%r,%r,%r','%r,%r,%r,%r'
 print formatter % (formatter, formatter, formatter, formatter)
-# prints all 4 strings 
+# prints all 4 strings
 print formatter % (
-    "I had this thing.",
-    "That you could type up right.",
+    'I had this thing.',
+    'That you could type up right.',
     "But it didn't sing",
-    "So I said goodnight."
+    'So I said goodnight.'
     )
+
+# Study Drills
+# 1. Do your checks of your work, write down your mistakes, and try not to make
+# them on the next exercise.
+# 2. Notice that the last line of output uses both single-quotes and double-quotes
+# for individual pieces. Why do you think that is?
+# The reason why both single quotes and double quotes were used was because,
+# to make sure that it stays a line of code and doesn't break or break
+# into seperate lines. " ' "
